74a8667b269a0c55922d37440f7c27a6bb3773b8
Sophie Chen <sophiexin036@gmail.com>
2019-01-03 16:57:06 +0800

    Create UVa#256-Quirksome-Squares.cpp

---
+++ UVa#256-Quirksome-Squares.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+	int d;
+	int i,j,k;
+	while(cin >> d){
+		if(d==0) break;
+		int digit[8]={0};
+		int v = 1;
+		for(i=0; i<d; i++) v *= 10;
+		for(i=0; i<v; i++){
+			int temp = i;
+			int a=0, b=0;
+			for(j=d-1; j>=0; j--){
+				digit[j] = temp%10;
+				temp /= 10;
+			}
+			for(j=0; j<d/2; j++){
+				a *= 10;
+				a += digit[j];
+			}
+			for(j=d/2; j<d; j++){
+				b *= 10;
+				b += digit[j];
+			}
+			if( (a+b)*(a+b) == i ){
+				for(j=0; j<d; j++)	printf("%d", digit[j]);
+				printf("\n");
+			}
+		}
+	}
+	return 0;
+}

72f3e56893f80961bab7ceb7ed22133fdd8f1530
Sophie Chen <sophiexin036@gmail.com>
2019-01-16 22:05:47 +0800

    Create UVa# 10008 What's Cryptanalysis.cpp
    2019/1/16

---
+++ UVa# 10008 What's Cryptanalysis.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <cstdio>
+#include <algorithm>
+#include <string>
+#include <cstring>
+#include <cctype>
+using namespace std;
+
+typedef struct TimeOfLetter{
+	char letter;
+	int num;
+}Letter;
+
+int main(){
+	int n;
+	char buf;
+	cin >> n;
+	buf = cin.get();
+	if(buf == ' ') cin.get();
+	int a[91] = {0};
+	Letter b[91];
+	
+	/* init */
+	for(int i=65; i<=91; i++){
+		b[i].letter = i;
+		b[i].num = 0;
+	}
+
+	for(int i=0; i<n; i++){
+		string s;
+		getline(cin, s);
+		for(int j=0; s[j] != '\0'; j++){
+			a[toupper(s[j])]++;
+		}
+	}
+	
+	for(int i=65; i<=90; i++){
+		b[i].num = a[i];
+	}
+
+	/* sort */
+	for(int i=65; i<90; i++){
+		for(int j=65; j<90; j++){
+			if(b[j].num < b[j+1].num){
+				char c_temp = b[j].letter;
+				b[j].letter = b[j+1].letter;
+				b[j+1].letter = c_temp;
+				
+				int temp = b[j].num;
+				b[j].num = b[j+1].num;
+				b[j+1].num = temp;
+			}
+		}
+	}
+	
+	for(int i=65; i<=90; i++){
+		if(b[i].num != 0)
+			printf("%c %d\n", b[i].letter, b[i].num);
+	}
+
+	return 0;
+}
